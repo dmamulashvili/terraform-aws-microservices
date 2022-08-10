2f02f00898992f9acf9776ea7f9ed6dc7ebb88f9
Dimitri Mamulashvili <dmamulashvili@protonmail.com>
2022-08-10 22:40:28 +0400

    Add profile to provider config and comment access_key & secret_key

--- src/main.tf
+++ src/main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
+#  access_key = var.access_key
+#  secret_key = var.secret_key
+  profile = ""
 }
 
 locals {
