ab60c9db89c546e0c259af6e237f379fcb1f3adb
Dimitri Mamulashvili <dmamulashvili@protonmail.com>
2022-08-10 22:50:45 +0400

    Add NAT info

--- src/main.tf
+++ src/main.tf
@@ -31,7 +31,7 @@ module "vpc" {
   cidr_block = "10.0.0.0/16"
 
   public_subnets_dmz = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  #  private_subnets_app = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
+  #  private_subnets_app = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"] # Single NAT in 1a zone
   #  private_subnets_res = ["10.0.111.0/24","10.0.112.0/24","10.0.113.0/24"]
 
   tags = local.tags
